7989b9ef79eabfdcb53cb98915d378ebb12aaf4b
Nameless <truemedian@gmail.com>
2021-08-16 16:07:52 -0500

    simplify templating

--- src/main.zig
+++ src/main.zig
@@ -3,38 +3,12 @@
 
 const std = @import("std");
 
-const AutoIndentingStream = @import("auto_indenting_stream.zig").AutoIndentingStream;
 const js = @import("javascript.zig");
-
-const pre = @embedFile("pre.js");
-const post = @embedFile("post.js");
 
 pub fn bind(namespace: anytype, writer: std.fs.File.Writer) anyerror!void {
     var bindgen = js.BindingGenerator.init(writer);
 
-    try writer.writeAll(pre);
-    _ = try writer.writeAll(
-        \\/**
-        \\ * @param {WebAssembly.Instance} instance
-        \\ */
-        \\module.exports = function bind(instance) {
-        \\    let dataView = new DataView(instance.exports.memory.buffer);
-        \\    function getDataView() {
-        \\        if (dataView.buffer.byteLength === 0) {dataView = new DataView(instance.exports.memory.buffer)} // Detached buffer (resizes cause this)
-        \\        return dataView;
-        \\    }
-        \\
-    );
-    bindgen.stream.pushIndent();
     try bindgen.generate(namespace);
-    bindgen.stream.popIndent();
-
-    try writer.print(
-        \\    return {s};
-        \\}}
-        \\
-    , .{@typeName(namespace)});
-    try writer.writeAll(post);
 }
 
 pub fn wasmExports(namespace: anytype) type {
